8e2dad39529d985571d35c63308972af745a93b7
nirtoren <nirtoren@ymail.com>
2024-06-18 08:05:33 +0300

    Added fuzzy search tests

--- internal/matcher/fuzzy.go
+++ internal/matcher/fuzzy.go
@@ -8,8 +8,6 @@ import (
 	"github.com/lithammer/fuzzysearch/fuzzy"
 )
 
-type MinFunc func([]PathDist) []PathDist
-
 type PathDist struct {
 	Path        string
 	LevDistance int
@@ -32,11 +30,7 @@ func FuzzyFind(path string, records []*record.PathRecord) ([]PathDist, error) {
 		basePaths = append(basePaths, k)
 	}
 
-	// var foundPaths []string = fuzzy.Find(path, basePaths) //Should get a list of optional paths
-
 	var rankedPaths fuzzy.Ranks = fuzzy.RankFind(path, basePaths)
-
-	// fmt.Println(rankedPaths)
 
 	if rankedPaths.Len() == 0 {
 		return []PathDist{}, errors.New("could not find suitable path") // In case could not find any match
@@ -54,21 +48,6 @@ func FuzzyFind(path string, records []*record.PathRecord) ([]PathDist, error) {
 			TwoMinDistances = append(TwoMinDistances, PathDist{Path: pathMap[path.Target], LevDistance: path.Distance})
 		}
 		return []PathDist{TwoMinDistances[0], TwoMinDistances[1]}, nil
-		// levenshteinPaths := FindLevenshteinDistance(path, foundPaths, findTwoMin, pathMap) // Should get a list of two elements for future score filtering
-		// return levenshteinPaths, nil
 	}
 
-
-	// if len(foundPaths) == 0 {
-	// 	// Should exit the program with a message to the user
-	// 	return []PathDist{}, errors.New("could not find suitable path") // In case could not find any match
-
-	// } else if len(foundPaths) == 1 {
-	// 	singlePath := PathDist{Path: pathMap[foundPaths[0]], LevDistance: 0}
-	// 	return []PathDist{singlePath}, nil // In case only one match was found, no need to procced to Levenshtein OR Score filters
-
-	// } else { // In case more then one match was found
-	// 	levenshteinPaths := FindLevenshteinDistance(path, foundPaths, findTwoMin, pathMap) // Should get a list of two elements for future score filtering
-	// 	return levenshteinPaths, nil
-	// }
 }
